02f36b9acb55031adf5351e9c5826e623e07a408
miketreml <miketreml@mac.com>
2026-04-29 16:51:13 -0500

    wave

--- library/specializations/common-utilities/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @process specializations/common-utilities
- * @description Common reusable utilities for babysitter process composition
- */
-
-export { convertToDocxTask } from './docx-conversion.js';
-export { fanOutFanIn, pipeline } from './parallel-combinator.js';
